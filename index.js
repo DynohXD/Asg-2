8e5fcabac3b04f34393f1e3268d9e5c31f2dda96
ongchenyu@gmail.com <ongchenyu7@gmail.com>
2023-02-08 23:39:33 +0800

    fetched store api + more lottie: loading products

--- index.js
+++ index.js
@@ -55,6 +55,8 @@ for (let i = 0; i < accordionBtn.length; i++) {
 }
 
 
+
+
 // slider for banner
 const sliderContainer = document.querySelector('.slider-container');
 const sliderItems = document.querySelectorAll('.slider-item');
@@ -70,8 +72,70 @@ const nextSlide = () => {
 sliderItems[0].style.display = 'flex';
 setInterval(nextSlide, intervalTime);
 
-/*-------------------------------*/
+
+
+
+
+// hide lottie animation when page is no longer loading for 2.65s
 setTimeout(function() {
   document.querySelector('.lottie-container').style.display = 'none';
   document.querySelector('.content').style.display = 'block';
 }, 2650);
+
+
+
+
+
+// fetch api from fake api store
+document.addEventListener('DOMContentLoaded', function() {
+  let products = document.querySelector('.products');
+  let url = `https://api.escuelajs.co/api/v1/products?offset=0&limit=5`;
+  let opset = 0;
+  async function fetchProducts(url) {
+      try {
+          let data = await fetch(url);
+          opset = opset + 5;
+          if (data.ok) {
+              let response = await data.json();
+
+              for (let i = 0; i < 55; i++) {
+                  let description = response[i].description;
+                  let title = response[i].title;
+                  products.innerHTML += `
+     <div class="product">
+         <img src="${response[i].images[1]}" alt="${
+          response[i].category.name
+        }" class="product-img">
+         <div class="product-content">
+         <h2 class="product-title">${title.length > 50 ? title.substring(0, 50).concat(' ...') : title}</h2>
+         <h4 class="product-category">${response[i].category.name}</h4>
+         <p class="product-description">${description.length > 100 ? description.substring(0, 100).concat(' ...more') : description}</p>
+         <div class="product-price-container">
+             <h3 class="product-price">$${response[i].price}</h3>
+             <a href="#!" data-productId="${response[i].id}" class="add-to-like"><ion-icon name="heart"></ion-icon></a>
+             <a href="#!" data-productId="${response[i].id}" class="add-to-cart"><ion-icon name="cart-outline"></ion-icon></a>
+         </div>
+         </div>
+        
+     </div>
+     `;
+              }
+          }
+      } catch (err) {
+          console.log(err);
+      }
+  }
+  fetchProducts(url);
+
+// lottie animation at the bottom
+  window.addEventListener('scroll', function() {
+      const { scrollTop, scrollHeight, clientHeight } = document.documentElement;
+      if (scrollTop + clientHeight >= scrollHeight - 1/4) {
+          setTimeout(() => {
+              fetchProducts(
+                  `https://api.escuelajs.co/api/v1/products?offset=${opset}&limit=5`
+              );
+          }, 1000);
+      }
+  });
+});
